64133b20754b96fd000945cf839e30fc664b940a
Inna Didenko <innadidenko29@gmail.com>
2025-07-08 23:14:27 +0300

    Updates

---
+++ static/js/2896.5c5932ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknola=self.webpackChunknola||[]).push([[2896],{2896:(e,s,n)=>{n.r(s),n.d(s,{default:()=>a});var r=n(579);const a=()=>(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Password recovery"}),(0,r.jsx)("p",{children:"Enter your username or email address and select Send Email"}),(0,r.jsx)("button",{children:"Send email"})]})}}]);
+//# sourceMappingURL=2896.5c5932ef.chunk.js.map
