633b0f3a206e045a14b407ad34f2ed43a0278262
Inna Didenko <innadidenko29@gmail.com>
2025-08-02 17:57:18 +0300

    add logo

--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import css from "./LoadingPage.module.css";
 import WelcomePage from "../WelcomePage/WelcomePage";
+import logo2 from "../../assets/images/nola-no-one-likes-advertisement-typography-1-Photoroom.png"
+import logo from "../../assets/images/nola.webp"
+import logo3 from "../../assets/images/nola-no-one-likes-advertisement-typography-1-Photoroom.jpg"
+import logo4 from "../../assets/images/nola-no-one-likes-advertisement-typography-1.jpeg"
+import logo5 from "../../assets/images/nola-no.svg"
+import logo6 from "../../assets/images/nola-logo.jpeg"
 
 const LoadingPage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 1500);
+  // setTimeout(() => {
+  //   setIsLoading(false);
+  // }, 6000
+  //   // 1500
+  // );
 
   return (
     <div>
       {isLoading ? (
         <div className={css.container}>
-          <div className={css.logo}>
-            <h1>NOLA</h1>
-          </div>
+        <div className={css.test}><p> webp <img src={logo} alt="logo" className={css.logo} /></p></div>
+          <div className={css.test}> <p>  png   <img src={logo2} alt="logo" className={css.logo} /></p></div>
+          {/* <div className={css.test}> <p>  jpeg   <img src={logo6} alt="logo" className={css.logo} /></p></div> */}
+          {/* <div className={css.test}> <p>  svg   <img src={logo5} alt="logo" className={css.logo}/></p></div> */}
         </div>
       ) : (
         <WelcomePage />
